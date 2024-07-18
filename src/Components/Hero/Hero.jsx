5b26be0ec69ed45210f3501eb8e11df538bc7d38
arch0123 <architshukla527@gmail.com>
2024-07-18 12:14:36 +0530

    Initial Commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import './Hero.css'
-// import My_Pic from '../../assets/My_Pic.png'
-
-// const Hero = () => {
-//   return (
-//     <div className='hero'>
-//         <img src={My_Pic} alt =''/>
-//       <h1><span>I'm Archit Shukla,</span>
-//       Web Developer based in India</h1>
-//       <p>I'm a passionate web developer with some key skills of HTML,CSS,JavaScript & Java and I also have a knowledge of React Js </p>
-//       <div className = 'hero-action'>
-//       <div className = 'hero-connect'>Connect With Me </div>
-//       <div className = 'hero-resume'>My Resume</div> 
-    
-//       </div>
-//     </div>
-//   )
-// } 
-
-
-
-// export default Hero 
-
 
 
 import React from 'react'
@@ -46,7 +22,7 @@ const Hero = () => {
       </p>
       <div className='hero-action'>
         <div className='hero-connect'>Connect With Me</div>
-        <button className='hero-resume' onClick={openResume}>My Resume</button>
+        <button className='hero-resume' onClick={openResume}>Resume</button>
       </div>
     </div>
   )
