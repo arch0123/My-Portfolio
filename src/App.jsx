12cc917961a341864f2026d9a39cde3f498146fc
arch0123 <architshukla527@gmail.com>
2024-07-18 12:45:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Services  from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer  from './Components/Footer/Footer'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import ResumePage from './Pages/ResumePage';
 
